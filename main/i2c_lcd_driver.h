fb38f88644db8feb8cd6d81d77c51849c5f9b7cf
Abil <abilrizky04@gmail.com>
2026-04-02 21:36:11 +0700

    i2c lcd header

---
+++ main/i2c_lcd_driver.h
@@ -0,0 +1,24 @@
+#ifndef I2C_LCD_DRIVER_H
+#define I2C_LCD_DRIVER_H
+
+#include "esp_err.h"
+#include "driver/i2c_master.h"
+
+esp_err_t i2c_master_init(void);
+
+void lcd_init(void);
+
+void lcd_put_cur(int row, int col);
+
+void lcd_send_string (char *str);
+
+void lcd_send_float(float num, int decimal_places);
+
+void lcd_send_int(int num);
+
+void lcd_clear(void);
+
+void lcd_clear_row(int row);
+
+
+#endif
